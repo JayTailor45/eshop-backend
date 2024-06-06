ae1d69d35f6790ea57f4b867276ecd7e3268b62c
Jay Tailor <tailorj64@gmail.com>
2024-06-06 18:12:34 +0530

    setup express server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello')
+const express = require('express');
+const morgan = require('morgan');
+
+require('dotenv/config');
+
+const app = express();
+
+const PREFIX = process.env.API_PREFIX || '/api';
+const PORT = process.env.SERVER_PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.get(`${PREFIX}/v1`, (req, res) => {
+    res.send({message: 'Hello World!'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is up and listening on port ${PORT}!`);
+});
