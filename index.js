8a020893d388a47a9f4efa7c2aec189caeff984a
Jay Tailor <tailorj64@gmail.com>
2024-06-06 20:21:57 +0530

    setup mongodb connection and mongodb express admin interface using docker compose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
 
@@ -15,6 +16,14 @@ app.use(morgan('tiny'));
 app.get(`${PREFIX}/v1`, (req, res) => {
     res.send({message: 'Hello World!'});
 });
+
+mongoose.connect(`${process.env.MONGODB_URL}?directConnection=true&authSource=admin&retryWrites=true`, {
+    dbName: process.env.MONGODB_DB_NAME,
+})
+    .then(() => {
+        console.log('Connected to MongoDB...');
+    })
+    .catch(err => console.log(err));
 
 app.listen(PORT, () => {
     console.log(`Server is up and listening on port ${PORT}!`);
